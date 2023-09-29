0d98eb77edb94aaf088d26cac1e917c1b95f2366
skumar28 <skumar28@oceaneering.com>
2023-09-29 15:13:12 +0530

    removed useless constructor.

--- src/helpers/base-middleware.ts
+++ src/helpers/base-middleware.ts
@@ -1,9 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default abstract class BaseMiddleware {
-  constructor() {
-    this.execute = this.execute.bind(this);
-  }
 
   public abstract execute(
     _req: Request,
